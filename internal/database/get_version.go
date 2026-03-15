93db1d08350a3486e62cd311359758264c2eb068
IsaacDSC <isaac8.silva@hotmail.com>
2026-03-15 09:17:58 -0300

    Update GetVersion function to retrieve migration count instead of the last version. This change improves the accuracy of version tracking by counting active migrations without rollback.

--- internal/database/get_version.go
+++ internal/database/get_version.go
@@ -9,7 +9,7 @@ import (
 // GetVersion returns the last version of the migrations table
 func GetVersion(db *sql.DB) (int, error) {
 	var dbVersion int
-	err := db.QueryRow("SELECT version FROM migrations WHERE rollback = FALSE ORDER BY version DESC LIMIT 1").Scan(&dbVersion)
+	err := db.QueryRow("SELECT count(version) FROM migrations WHERE rollback = FALSE;").Scan(&dbVersion)
 	if err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return 0, fmt.Errorf("failed to get version: %w", err)
 	}
